be319303ab5151beca162eb3b474d627c6331526
NITKRacing-Electric <87520877+NITKRacing-Electric@users.noreply.github.com>
2021-07-17 12:44:00 +0530

    Add files via upload

---
+++ Battery-BMS/BatteryModel_v1/graph.m
@@ -0,0 +1,4 @@
+plot(time,SoC,'LineWidth',2);
+title('State of Charge(SoC) Vs Time','fontweight','bold','fontsize',16);
+xlabel('Time','fontweight','bold','fontsize',12);
+ylabel('State of Charge(SoC)','fontweight','bold','fontsize',12);
